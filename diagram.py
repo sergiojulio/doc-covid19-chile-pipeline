f6058db1de6f185a1eecd81ff623c0688c6c36f1
sergiojulio <sergiojulio@gmail.com>
2023-01-22 18:10:32 -0300

    driagram name

--- diagram.py
+++ diagram.py
@@ -4,5 +4,5 @@ from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
 
-with Diagram("pipeline", show=False):
+with Diagram("pipeline-cloud-run", show=False):
     ELB("lb") >> EC2("web") >> RDS("userdb")
