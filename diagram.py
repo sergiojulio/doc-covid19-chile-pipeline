2cb838a2065d5043ee673cee54d77fa13f21319a
sergiojulio <sergiojulio@gmail.com>
2023-01-29 20:40:28 -0300

    changes!

--- diagram.py
+++ diagram.py
@@ -3,6 +3,7 @@ from diagrams import Diagram
 from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
+from diagrams.gcp.compute import Run
 
 with Diagram("pipeline-cloud-run", show=False):
-    ELB("lb") >> EC2("web") >> RDS("userdb")
+    ELB("lb") >> Run("web") >> RDS("userdb")
