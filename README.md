4382a958d29d64a0e8e4c8fd8ec7af1af72aaad1
sergiojulio <sergiojulio@gmail.com>
2023-01-30 16:47:08 -0300

    Changes!

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 This is a documentation
 
 Here an architecture diagram
+![Alt text](covid_19_chile_etl_architecture.png "architecture diagram")
--- diagram.py
+++ diagram.py
@@ -1,9 +1,28 @@
  
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
-from diagrams.gcp.compute import Run
+# Secret Manager
+# Container Registry
+# Cloud Scheduler
+# Cloud Run
 
-with Diagram("pipeline-cloud-run", show=False):
-    ELB("lb") >> Run("web") >> RDS("userdb")
+from diagrams import Cluster, Diagram
+from diagrams.aws.compute import ECS
+from diagrams.aws.database import RDS
+from diagrams.aws.network import Route53
+
+with Diagram("Covid 19 Chile Etl architecture", show=False):
+    
+    github = ECS("Github")
+
+    with Cluster("Google Cloud Platform"):
+
+        cloud_scheduler = RDS("Cloud Scheduler")
+
+        with Cluster("Execution"):
+
+            cloud_run = RDS("Cloud Run")
+            cloud_run << [RDS("Secret Manager"), RDS("Container Registry")]
+
+    cloud_scheduler >> cloud_run
+
+    cloud_run >> github 
+    cloud_run << github               
